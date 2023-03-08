6c5292200a68912b522d8b0629f0b6e6637cde6a
HyukJu <HyukJu@HyukJu.local>
2023-03-08 15:47:31 +0900

    [43기 강혁주] 데이터베이스 테이블 생성 - 수정 2]

--- db/migrations/20230308042746_carts.sql
+++ db/migrations/20230308042746_carts.sql
@@ -2,12 +2,10 @@
 CREATE TABLE carts(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   quantity INT NOT NULL,
-  product_id INT NOT NULL,
   user_id INT NOT NULL,
   product_option_id INT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  CONSTRAINT carts_products_product_id FOREIGN KEY (product_id) REFERENCES products(id),
   CONSTRAINT carts_users_user_id FOREIGN KEY (user_id) REFERENCES users(id),
   CONSTRAINT carts_product_options_product_option_id FOREIGN KEY (product_option_id) REFERENCES product_options(id)
 )
