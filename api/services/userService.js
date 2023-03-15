cbd8f2d6d34778ed5d04c19e628386d2c6342b7d
SUN <sun@SUNui-MacBookPro.local>
2023-03-15 10:30:38 +0900

    [문선정] 회원가입 및 로그인

--- api/services/userService.js
+++ api/services/userService.js
@@ -20,25 +20,6 @@ const signUp = async (
   await emailValidate(email);
   await passwordValidate(password);
 
-  // const emailRegex =
-  //   /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-  // const passwordRegex =
-  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-
-  // if (!emailRegex.test(email)) {
-  //   const error = new Error("INVALID_EMAIL");
-  //   error.statusCode = 400;
-
-  //   throw error;
-  // }
-
-  // if (!passwordRegex.test(password)) {
-  //   const error = new Error("INVALID_PASSWORD");
-  //   error.statusCode = 400;
-
-  //   throw error;
-  // }
-
   const hashedPassword = await hashPassword(password);
 
   return userDao.createUser(
@@ -51,25 +32,6 @@ const signUp = async (
 };
 
 const signIn = async (email, password) => {
-  const emailRegex =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-  const passwordRegex =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-
-  if (!emailRegex.test(email)) {
-    const error = new Error("INVALID_EMAIL");
-    error.statusCode = 401;
-
-    throw error;
-  }
-
-  if (!passwordRegex.test(password)) {
-    const error = new Error("INVALID_PASSWORD");
-    error.statusCode = 401;
-
-    throw error;
-  }
-
   const user = await userDao.getUserByEmail(email);
 
   if (!user) {
